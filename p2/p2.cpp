e46784be1530dff016ab1e369234298f9aa08eaa
jung <agppg@naver.com>
2024-11-17 03:08:15 +0900

    수정

--- p2/p2.cpp
+++ p2/p2.cpp
@@ -127,7 +127,7 @@ int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("3D Shapes with Transformations");
+    glutCreateWindow("ComputerGraphics2_2");
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
     glutKeyboardFunc(keyboard);
