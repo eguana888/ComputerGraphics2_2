2dbcb20dca839b20189b884749d6415466df4abb
jung <agppg@naver.com>
2024-11-17 02:41:12 +0900

    초기 커밋.

---
+++ p2/p2.cpp
@@ -0,0 +1,138 @@
+﻿#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glu.h>
+#include <gl/glut.h>
+
+float angleX = 0.0f;
+float angleY = 0.0f;
+float scale = 1.0f;
+float translateZ = -5.0f;
+
+float cameraAngleX = 0.0f;
+float cameraAngleY = 0.0f;
+float cameraDistance = 10.0f;
+
+int viewX = 400;
+int viewY = 300;
+
+void initGL() {
+    glEnable(GL_DEPTH_TEST);
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+
+    GLfloat cameraX = (viewX - 400) / 200.0f;
+    GLfloat cameraY = (300 - viewY) / 200.0f;
+    gluLookAt(cameraX, cameraY, cameraDistance,
+        0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f);
+
+    glTranslatef(0.0f, 0.0f, translateZ);
+    glScalef(scale, scale, scale);
+    glRotatef(angleX, 1.0f, 0.0f, 0.0f);
+    glRotatef(angleY, 0.0f, 1.0f, 0.0f);
+
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+
+    glColor3f(1.0f, 0.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+
+    glColor3f(0.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h) {
+    if (h == 0) h = 1;
+    float aspect = (float)w / (float)h;
+
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    switch (key) {
+    case 'q':
+        translateZ += 0.1f;
+        break;
+    case 'e':
+        translateZ -= 0.1f;
+        break;
+    case 'w':
+        angleX += 0.2f;
+        break;
+    case 's':
+        angleX -= 0.2f;
+        break;
+    case 'a':
+        angleY -= 2.0f;
+        break;
+    case 'd':
+        angleY += 2.0f;
+        break;
+    case '+':
+        scale += 0.1f;
+        break;
+    case '-':
+        scale -= 0.1f;
+        break;
+    }
+
+    glutPostRedisplay();
+}
+
+void MyMouseMove(GLint X, GLint Y) {
+    viewX = X*3;
+    viewY = Y*3;
+    glutPostRedisplay();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("3D Shapes with Transformations");
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutMotionFunc(MyMouseMove);
+    initGL();
+    glutMainLoop();
+    return 0;
+}
